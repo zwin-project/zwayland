8667636fc811854deee0c0daeea70d79e19c6645
Aki-7 <aki.develop8128@gmail.com>
2021-08-21 21:13:13 +0900

    Use "z11-0" as a default name of a socket to connect

--- zwc/zwc_display.c
+++ zwc/zwc_display.c
@@ -1,86 +1,15 @@
-#define _GNU_SOURCE
-
-#include <assert.h>
-#include <errno.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <unistd.h>
 #include <wayland-client.h>
 #include <zwc.h>
 
 #include "zwc_global.h"
-#include "zwc_os.h"
 
 struct zwc_display {
   struct wl_display* wl_display;
   struct zwc_global* global;
-  int fd;
   struct z11_virtual_object* virtual_object;
 };
-
-static int connect_to_socket(const char* name)
-{
-  struct sockaddr_un addr;
-  socklen_t size;
-  const char* runtime_dir;
-  int name_size, fd;
-  bool path_is_absolute;
-
-  if (name == NULL) name = getenv("Z11_WAYLAND_DISPLAY");
-  if (name == NULL) name = "wayland-0";
-
-  path_is_absolute = name[0] == '/';
-
-  runtime_dir = getenv("Z11_XDG_RUNTIME_DIR");
-  if (!runtime_dir && !path_is_absolute) {
-    fprintf(stderr, "zwc error: Z11_XDG_RUNTIME_DIR not set in the environment.\n");
-    return -1;
-  }
-
-  fd = zwc_os_socket_cloexec(PF_LOCAL, SOCK_STREAM, 0);
-  if (fd < 0) return -1;
-
-  memset(&addr, 0, sizeof addr);
-  addr.sun_family = AF_LOCAL;
-  if (!path_is_absolute) {
-    name_size = snprintf(addr.sun_path, sizeof addr.sun_path, "%s/%s", runtime_dir, name) + 1;
-  } else {
-    /* absolute path */
-    name_size = snprintf(addr.sun_path, sizeof addr.sun_path, "%s", name) + 1;
-  }
-
-  assert(name_size > 0);
-  if (name_size > (int)sizeof addr.sun_path) {
-    if (!path_is_absolute) {
-      fprintf(stderr,
-              "zwc error: socket path \"%s/%s\" plus null terminator"
-              " exceeds %i bytes\n",
-              runtime_dir, name, (int)sizeof(addr.sun_path));
-    } else {
-      fprintf(stderr,
-              "zwc error: socket path \"%s\" plus null terminator"
-              " exceeds %i bytes\n",
-              name, (int)sizeof(addr.sun_path));
-    }
-    close(fd);
-    /* to prevent programs reporting
-     * "failed to add socket: Success" */
-    errno = ENAMETOOLONG;
-    return -1;
-  };
-
-  size = offsetof(struct sockaddr_un, sun_path) + name_size;
-
-  if (connect(fd, (struct sockaddr*)&addr, size) < 0) {
-    close(fd);
-    return -1;
-  }
-
-  return fd;
-}
 
 static void callback_done(void* data, struct wl_callback* callback, uint32_t callback_data);
 
@@ -104,13 +33,13 @@ struct zwc_display* zwc_display_create(const char* name)
   struct zwc_display* zwc_display;
   struct z11_virtual_object* virtual_object;
   struct wl_callback* cb;
-  int fd = connect_to_socket(name);
-  if (fd < 0) goto fail;
+
+  if (name == NULL) name = "z11-0";
 
   zwc_display = calloc(1, sizeof *zwc_display);
   if (zwc_display == NULL) goto fail;
 
-  zwc_display->wl_display = wl_display_connect_to_fd(fd);
+  zwc_display->wl_display = wl_display_connect(name);
   if (zwc_display->wl_display == NULL) {
     fprintf(stderr, "zwc error: can't connect to display\n");
     goto out_zwc_display;
@@ -120,8 +49,6 @@ struct zwc_display* zwc_display_create(const char* name)
   if (zwc_display->global == NULL) {
     goto out_wl_display;
   }
-
-  zwc_display->fd = fd;
 
   virtual_object = z11_compositor_create_virtual_object(zwc_display->global->compositor);
 
@@ -168,4 +95,4 @@ int zwc_display_read_events(struct zwc_display* display)
   return wl_display_read_events(display->wl_display);
 }
 
-int zwc_display_get_fd(struct zwc_display* display) { return display->fd; }
+int zwc_display_get_fd(struct zwc_display* display) { return wl_display_get_fd(display->wl_display); }
--- zwc/zwc_os.h
+++
@@ -1,6 +0,0 @@
-#ifndef ZWC_OS_H
-#define ZWC_OS_H
-
-int zwc_os_socket_cloexec(int domain, int type, int protocol);
-
-#endif  //  ZWC_OS_H
